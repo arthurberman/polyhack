970faec2862d87a64b68e3687272382ac5cde10a
Arthur Berman <arthurberman94@gmail.com>
2014-10-24 23:30:14 -0400

    session management go

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, send_from_directory
+from flask import Flask, request, render_template, redirect, url_for, send_from_directory, session, make_response
 from werkzeug import secure_filename
 import flask
 import os
@@ -8,6 +8,7 @@ import json
 ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "uploads"
+app.secret_key = "ThomasWroteSOMETHINGCOOL"
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -33,10 +34,26 @@ def image():
                  <input type=submit value=Upload>
             </form>
             '''
-
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session['code'] = request.form['code']
+        response = make_response(redirect("/check"))
+        response.set_cookie('code', session['code'])
+        return response
+    else:
+        pass
+    return render_template("login.html")
+@app.route('/check')
+def check():
+    return render_template("check.html",  items=getItems(request.cookies["code"]))
+def getItems(cookie):
+    return [cookie]
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
+
 if __name__=="__main__":
     app.run(debug=True)
